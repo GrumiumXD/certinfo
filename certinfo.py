91f21dfa3cb98ca0f777483b4c02b4f7ca740277
Gunnar Raßmann <g_rassmann@mail.de>
2023-05-31 01:58:00 +0200

    Add remote PEM certificate filtering

--- certinfo.py
+++ certinfo.py
@@ -20,14 +20,37 @@ def get_remote_certificates(host_name, port=443):
             "--showcerts",
         ],
         capture_output=True,
+        text=True,
         stdin=subprocess.DEVNULL,
+    )
+
+    certificates = []
+    cert = ""
+    for line in result.stdout.splitlines():
+        if line == "-----BEGIN CERTIFICATE-----":
+            cert = ""
+        cert += f"{line}\n"
+        if line == "-----END CERTIFICATE-----":
+            certificates.append(cert)
+
+    return certificates
+
+
+def print_cert_info(cert):
+    result = subprocess.run(
+        ["openssl", "x509", "--subject", "--noout"],
+        capture_output=True,
+        text=True,
+        input=cert,
     )
 
     print(result.stdout)
 
 
 def main():
-    get_remote_certificates("google.de")
+    certs = get_remote_certificates("google.de")
+    for c in certs:
+        print_cert_info(c)
 
 
 if __name__ == "__main__":
